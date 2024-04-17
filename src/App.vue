a2b8b6c52c268a9293b59af7b05f82ebdd8b3191
Zhenbiao Huang <biao876990970@hotmail.com>
2024-04-17 10:09:29 +0800

    set initial device ip automatically

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeMount } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useControlParams } from 'stores/control-params'
 
@@ -13,6 +13,18 @@ export default defineComponent({
     const { locale } = useI18n({ useScope: 'global' })
     const params = useControlParams()
     locale.value = params.locale
+
+    onBeforeMount(() => {
+      let pos = window.location.href.indexOf('://')
+      let deviceIP
+      if (pos >= 0) {
+        pos += 3
+        deviceIP = window.location.href.slice(pos)
+        deviceIP = deviceIP.split(':')[0]
+        deviceIP = deviceIP.split('/')[0]
+        params.ip = deviceIP
+      }
+    })
   }
 })
 </script>
